1b720297ce4abd6eddcf561ff104a8f20689857b
Rakan Nimer <RakanNimer@users.noreply.github.com>
2016-10-19 12:34:53 +0300

    Added getLastKnownLocation and getCountryCode [iOS]

--- RNOpenSettings.m
+++ RNOpenSettings.m
@@ -13,12 +13,32 @@ RCT_EXPORT_METHOD(openAppPermissionsSettings) {
     [[UIApplication sharedApplication] openURL:[NSURL URLWithString:UIApplicationOpenSettingsURLString]];
 }
 
+RCT_EXPORT_METHOD(getCountryCode: (RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject) {
+    NSLocale *currentLocale = [NSLocale currentLocale];  // get the current locale.
+    NSString *countryCode = [currentLocale objectForKey:NSLocaleCountryCode];
+    resolve(countryCode);
+}
+
+
+RCT_EXPORT_METHOD(getLastKnownLocation: (RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject) {
+    CLLocationManager * locationManager = [[CLLocationManager alloc] init];
+    CLLocation * lastKnownLocation = [locationManager location];
+    NSString * result = [NSString stringWithFormat:@"%@", lastKnownLocation];
+    if (lastKnownLocation == nil){
+        resolve(@"none");
+    }
+    else {
+        resolve(lastKnownLocation);
+    }
+    
+}
+
 RCT_EXPORT_METHOD(isLocationEnabled: (RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject) {
     if([CLLocationManager locationServicesEnabled] &&
        [CLLocationManager authorizationStatus] != kCLAuthorizationStatusDenied) {
-        // NSString *a = @"1";
-        // BOOL *initialBroadcast = YES;
         resolve([NSNumber numberWithBool:YES]);
     } else {
         resolve([NSNumber numberWithBool:NO]);
